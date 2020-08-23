ad04d1e450e911593bce3834c2f9376d66384fa2
Yashika Bihani <yashikabihani7@gmail.com>
2020-08-24 02:50:43 +0530

    Basic Structure of the Application

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <h1>This is Home Page</h1>
+            <div className='home__header'>
+                <div className='home__headerLeft'>
+                    {/*About Link*/}
+                    {/*Store Link*/}
+                </div>
+                <div className='home__headerRight'>
+                    {/*Gmail Link*/}
+                    {/*Images Link*/}
+                    {/*App Collection Link*/}
+                    {/*Avatar Link*/}
+                </div>
+            </div>
+            <div className='home__body'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
